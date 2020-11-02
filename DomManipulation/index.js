03638858cfddd30527975cf3a74f7da20c82a168
Joel H <joel.hacke@gmail.com>
2020-11-02 14:07:21 -0600

    done with lesson, now on to practice

--- DomManipulation/index.js
+++ DomManipulation/index.js
@@ -68,3 +68,14 @@ function myEventHandler(event) {
 }
 
 btn2.addEventListener('click', myEventHandler)
+
+// buttons is a node list. It looks and acts much like an array.
+const buttons = document.querySelectorAll('button');
+// we use the .forEach method to iterate through each button
+buttons.forEach((button) => {
+  // and for each one we add a 'click' listener
+  button.addEventListener('click', () => {
+      alert(button.id)
+      //button.disabled = true;
+  });
+});
