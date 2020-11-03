7e5bc53ec7f3cf05754d62aea9d8e5466dd1b82a
Joel H <joel.hacke@gmail.com>
2020-11-03 15:13:42 -0600

    ran prettier

--- RockPaperScissors/index.js
+++ RockPaperScissors/index.js
@@ -2,96 +2,89 @@
 let playerScore = 0;
 let computerScore = 0;
 let roundsPlayed = 0;
+const scoreboard = document.querySelector(".score");
+const pscore = document.querySelector("#player-score");
+const cscore = document.querySelector("#computer-score");
 
-const buttons = document.querySelectorAll('.choice-button')
-buttons.forEach(button => button.addEventListener('click', game))
-
+const buttons = document.querySelectorAll(".choice-button");
+buttons.forEach((button) => button.addEventListener("click", game));
 
 function updateScore() {
-    const scoreboard = document.querySelector('.score');
-    const pscore = document.querySelector('#player-score');
-    const cscore = document.querySelector('#computer-score');
-    pscore.textContent = `Player: ${playerScore}`;
-    cscore.textContent = `Computer: ${computerScore}`;
-    scoreboard.appendChild(pscore);
-    scoreboard.appendChild(cscore);
-    
+  pscore.textContent = `Player: ${playerScore}`;
+  cscore.textContent = `Computer: ${computerScore}`;
+  scoreboard.appendChild(pscore);
+  scoreboard.appendChild(cscore);
 }
 /// end button stuff
 
 function game(e) {
-    const playerSelection = e.currentTarget.id
+  const playerSelection = e.currentTarget.id;
 
-    let computerSelection = computerPlay();
-    let result = playRound(playerSelection, computerSelection);
+  let computerSelection = computerPlay();
+  let result = playRound(playerSelection, computerSelection);
 
-    if (result) {
-        playerScore++;
-    } else if (isNaN(result)) {
-        roundsPlayed--;
-        console.log("Tie!");
-    } else 
-        computerScore++;
-    
-    updateScore()
-    
-    if (roundsPlayed === 5) {
-        if (playerScore > computerScore) {
-            alert("Player Wins!")
-        } else {
-            alert("Computer Wins!")
-        }
+  if (result) {
+    playerScore++;
+  } else if (isNaN(result)) {
+    roundsPlayed--;
+    console.log("Tie!");
+  } else computerScore++;
+
+  updateScore();
+
+  if (roundsPlayed >= 5) {
+    if (playerScore > computerScore) {
+      alert("Player Wins!");
     } else {
-        roundsPlayed++;
+      alert("Computer Wins!");
     }
+  } else {
+    roundsPlayed++;
+  }
 
-    console.log(`player score: ${playerScore}\ncomputer score: ${computerScore}`)
+  console.log(`player score: ${playerScore}\ncomputer score: ${computerScore}`);
 }
 
-
 function computerPlay() {
-    let random = Math.floor(Math.random() *3 );
-    let playerSelectionList= ["rock", "paper", "scissors"]
+  let random = Math.floor(Math.random() * 3);
+  let playerSelectionList = ["rock", "paper", "scissors"];
 
-    return playerSelectionList[random];
+  return playerSelectionList[random];
 }
 
 function playRound(playerSelection, computerSelection) {
-    if (playerSelection === computerSelection) {
-        return NaN
-    }
-    switch(playerSelection) {
-        case "rock":
-            if (computerSelection === "paper") {
-                return false
-            } else {
-                return true
-            }
-            break;
-        case "paper":
-            if (computerSelection === "scissors") {
-                return false
-            } else {
-                return true
-            }
-            break;
-        case "scissors":
-            if (computerSelection === 'rock') {
-                return false;
-            } else {return true}
-            break;
-    }
-    
+  if (playerSelection === computerSelection) {
+    return NaN;
+  }
+  switch (playerSelection) {
+    case "rock":
+      if (computerSelection === "paper") {
+        return false;
+      } else {
+        return true;
+      }
+      break;
+    case "paper":
+      if (computerSelection === "scissors") {
+        return false;
+      } else {
+        return true;
+      }
+      break;
+    case "scissors":
+      if (computerSelection === "rock") {
+        return false;
+      } else {
+        return true;
+      }
+      break;
+  }
 }
-
-
-
 
 //
 //button.addEventListener('click', (e) => {
 //    console.log(e);
 //})
-
 
 //console.log(game());
 
