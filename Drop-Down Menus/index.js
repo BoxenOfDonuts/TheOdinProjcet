c64c2b2e570bc68782b80ee116e1700bc51db61d
Joel H <joel.hacke@gmail.com>
2020-12-26 14:12:40 -0600

    prettier updates

--- Drop-Down Menus/index.js
+++ Drop-Down Menus/index.js
@@ -5,60 +5,61 @@
 // })
 
 // for mobile
-const navButtons = document.querySelectorAll('li');
-const body = document.querySelector('#content')
-document.querySelector('a').classList.add('active')
+const navButtons = document.querySelectorAll("li");
+const body = document.querySelector("#content");
+document.querySelector("a").classList.add("active");
 
 const displayController = (() => {
-    const _clearPage = () => {
-        while (body.firstElementChild) {
-            body.firstElementChild.remove()
-        }
+  const _clearPage = () => {
+    while (body.firstElementChild) {
+      body.firstElementChild.remove();
     }
+  };
 
-    const _updateActive = (e) => {
-        const elems = document.querySelectorAll('.active')
-        elems.forEach((el) => {
-            el.classList.remove('active')
-        })
-        e.target.classList.add('active')
-    }
+  const _updateActive = (e) => {
+    const elems = document.querySelectorAll(".active");
+    elems.forEach((el) => {
+      el.classList.remove("active");
+    });
+    e.target.classList.add("active");
+  };
 
-    const drawContent = (e, tab) => {
-        _updateActive(e)
-        _clearPage()
-        const fakeContent = document.createElement('p')
-        fakeContent.textContent = `this page is the ${tab} page`
-        body.appendChild(fakeContent)
+  const drawContent = (e, tab) => {
+    _updateActive(e);
+    _clearPage();
+    const fakeContent = document.createElement("p");
+    fakeContent.textContent = `this page is the ${tab} page`;
+    body.appendChild(fakeContent);
+  };
 
-    }
-
-    return { drawContent }
+  return { drawContent };
 })();
 
-navButtons.forEach(button => button.addEventListener('click', (e) => {
-    const link = e.target.hash
-    const active = document.querySelector('.active').hash
+navButtons.forEach((button) =>
+  button.addEventListener("click", (e) => {
+    const link = e.target.hash;
+    const active = document.querySelector(".active").hash;
 
     if (link === active) {
-        return
+      return;
     }
 
     //obviously this is uncessary for our short example
     switch (link) {
-        case '#home':
-            displayController.drawContent(e, link)
-            break
-        case '#poop':
-            displayController.drawContent(e, link)
-            break
-        case '#wizard':
-            displayController.drawContent(e, link)
-            break
-        case '#profile':
-            displayController.drawContent(e, link)
-            break
-        default:
-            displayController.drawContent(e, link)
+      case "#home":
+        displayController.drawContent(e, link);
+        break;
+      case "#poop":
+        displayController.drawContent(e, link);
+        break;
+      case "#wizard":
+        displayController.drawContent(e, link);
+        break;
+      case "#profile":
+        displayController.drawContent(e, link);
+        break;
+      default:
+        displayController.drawContent(e, link);
     }
-}))
+  })
+);
