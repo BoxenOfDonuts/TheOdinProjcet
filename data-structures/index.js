484797fa4af696530cccb134487085b8e5a9db57
Joel H <joel.hacke@gmail.com>
2021-02-22 22:17:22 -0600

    more testing

---
+++ data-structures/index.js
@@ -0,0 +1,7 @@
+import test from './test.js';
+
+const sum = (a, b) => {
+  return a + b;
+}
+
+module.exports = sum;
---
+++ data-structures/index.test.js
@@ -0,0 +1,7 @@
+import { hasUncaughtExceptionCaptureCallback } from "process"
+
+describe('Tests', () => {
+  it('Should return 1 + 1 = 2', () => {
+    
+  })
+})
